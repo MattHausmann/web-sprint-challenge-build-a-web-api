1d26f2112b9b246995e186bf9eef151300a4e8ba
Matt Hausmann <matthew.p.hausmann@gmail.com>
2024-04-21 11:55:24 -0500

    initial commit

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,34 @@
 // Write your "projects" router here!
+const express = require('express');
+const server = express();
+server.use(express.json());
+const router = express.Router();
+
+const db = require('./projects-model');
+
+const projects = [];
+
+router.get('/', (req, res) => {
+    console.log("about to get data");
+    db.get(projects).then((data) => {
+        res.status(200).json(data?data:[]);
+    });
+    console.log("should still be getting data");
+});
+
+router.post('/', (req, res) => {
+    console.log(req);
+    let projName = req.body.name;
+    let projDesc = req.body.description;
+    let projCompleted = req.body.completed;
+
+    if(!projName || !projDesc || !projCompleted) {
+        res.statusCode(400);
+    }
+    let proj= {name:projName,desc:projDesc,completed:projCompleted};
+    db.insert(proj).then((data) => {
+        res.status(200).json(data);
+    })
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const server = express();
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+const projectRoutes = require('./projects/projects-router');
+
+const server = express();
+
+server.use('/projects', projectRoutes);
+
 
 module.exports = server;
